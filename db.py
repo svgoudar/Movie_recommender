2e22053b74f85c5540e0164eef23e137a23919ac
svgoudar <48946832+svgoudar@users.noreply.github.com>
2020-12-31 22:42:59 +0530

    Update db.py

--- db.py
+++ db.py
@@ -11,7 +11,7 @@ def connection(app):
     #                               host="ec2-75-101-232-85.compute-1.amazonaws.com",
     #                               port="5432",
     #                               database="ddlj5rrgii4pdl")
-   connection = connect(user="ytxlxwlysehdbe",
+   conn = connect(user="ytxlxwlysehdbe",
                                   password="f48ba2aec3b7f09a41bc2d0b4d48644c202b01d7fd9499a54833c0be9282bf8d",
                                   host="ec2-75-101-232-85.compute-1.amazonaws.com",
                                   port="5432",
@@ -22,9 +22,9 @@ def connection(app):
     # app.config['MYSQL_DATABASE_PASSWORD'] = config.dbpassword
     # app.config['MYSQL_DATABASE_DB'] = config.dbname
     # app.config['MYSQL_DATABASE_HOST'] = config.dbhost
-    cursor = connection.cursor()
+    cursor = conn.cursor()
     # mysql.init_app(app)
     # conn = mysql.connect()
     # cursor = conn.cursor()
     # SQLAlchemy(app)
-    return cursor, connection
+    return cursor, conn
