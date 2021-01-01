571c5cbee8c8beac30a8dd38853f4a33efb74f0a
sanju-svg <sanjeevvgoudar@outlook.com>
2021-01-02 01:50:27 +0530

    kkkk

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import pandas as pd
 import bs4 as bs
 import urllib.request
 import pickle
+from datetime import date
 
 app = Flask(__name__)
 app.secret_key = "iamkey345"
@@ -35,6 +36,26 @@ cursor = conn.cursor()
 filename = 'nlp_model.pkl'
 clf = pickle.load(open(filename, 'rb'))
 vectorizer = pickle.load(open('tranform.pkl', 'rb'))
+
+def convert_to_list(my_list):
+    my_list = my_list.split('","')
+    my_list[0] = my_list[0].replace('["','')
+    my_list[-1] = my_list[-1].replace('"]','')
+    return my_list
+
+# convert list of numbers to list (eg. "[1,2,3]" to [1,2,3])
+def convert_to_list_num(my_list):
+    my_list = my_list.split(',')
+    my_list[0] = my_list[0].replace("[","")
+    my_list[-1] = my_list[-1].replace("]","")
+    return my_list
+
+def get_suggestions():
+    data = pd.read_csv('main_data.csv')
+    return list(data['movie_title'].str.capitalize())
+
+
+
 def create_similarity():
     data = pd.read_csv('main_data.csv')
     # creating a count matrix
@@ -44,8 +65,9 @@ def create_similarity():
     similarity = cosine_similarity(count_matrix)
     return data,similarity
 
+
 def rcmd(m):
-    # global similarity, data
+    global similarity, data
     m = m.lower()
     try:
         data.head()
@@ -66,28 +88,21 @@ def rcmd(m):
         return l
 
 # converting list of string to list (eg. "["abc","def"]" to ["abc","def"])
-def convert_to_list(my_list):
-    my_list = my_list.split('","')
-    my_list[0] = my_list[0].replace('["','')
-    my_list[-1] = my_list[-1].replace('"]','')
-    return my_list
+# def convert_to_list(my_list):
+#     my_list = my_list.split('","')
+#     my_list[0] = my_list[0].replace('["','')
+#     my_list[-1] = my_list[-1].replace('"]','')
+#     return my_list
+#
+# def convert_to_list_num(my_list):
+#     my_list = my_list.split(',')
+#     my_list[0] = my_list[0].replace("[","")
+#     my_list[-1] = my_list[-1].replace("]","")
+#     return my_list
 
 
 
 # @app.route("/")
-@app.route("/movierecommender")
-def movierecommender():
-    suggestions = get_suggestions()
-    # return render_template('movie_recommender_home.html', suggestions=suggestions,username=us)
-
-    return render_template('movie_recommender_home.html', suggestions=suggestions)
-
-
-def get_suggestions():
-    data = pd.read_csv('main_data.csv')
-    return list(data['movie_title'].str.capitalize())
-
-
 
 
 @app.route("/similarity", methods=["POST"])
@@ -118,16 +133,21 @@ def recommend():
     overview = request.form['overview']
     vote_average = request.form['rating']
     vote_count = request.form['vote_count']
+    rel_date = request.form['rel_date']
     release_date = request.form['release_date']
     runtime = request.form['runtime']
     status = request.form['status']
     rec_movies = request.form['rec_movies']
     rec_posters = request.form['rec_posters']
+    rec_movies_org = request.form['rec_movies_org']
+    rec_year = request.form['rec_year']
+    rec_vote = request.form['rec_vote']
 
     # get movie suggestions for auto complete
     suggestions = get_suggestions()
 
     # call the convert_to_list function for every string that needs to be converted to list
+    rec_movies_org = convert_to_list(rec_movies_org)
     rec_movies = convert_to_list(rec_movies)
     rec_posters = convert_to_list(rec_posters)
     cast_names = convert_to_list(cast_names)
@@ -138,16 +158,20 @@ def recommend():
     cast_places = convert_to_list(cast_places)
 
     # convert string to list (eg. "[1,2,3]" to [1,2,3])
-    cast_ids = cast_ids.split(',')
-    cast_ids[0] = cast_ids[0].replace("[", "")
-    cast_ids[-1] = cast_ids[-1].replace("]", "")
+    cast_ids = convert_to_list_num(cast_ids)
+    rec_vote = convert_to_list_num(rec_vote)
+    rec_year = convert_to_list_num(rec_year)
 
     # rendering the string to python string
     for i in range(len(cast_bios)):
         cast_bios[i] = cast_bios[i].replace(r'\n', '\n').replace(r'\"', '\"')
 
-    # combining multiple lists as a dictionary which can be passed to the html file so that it can be processed easily and the order of information will be preserved
-    movie_cards = {rec_posters[i]: rec_movies[i] for i in range(len(rec_posters))}
+    for i in range(len(cast_chars)):
+        cast_chars[i] = cast_chars[i].replace(r'\n', '\n').replace(r'\"', '\"')
+
+        # combining multiple lists as a dictionary which can be passed to the html file so that it can be processed easily and the order of information will be preserved
+    movie_cards = {rec_posters[i]: [rec_movies[i], rec_movies_org[i], rec_vote[i], rec_year[i]] for i in
+                   range(len(rec_posters))}
 
     casts = {cast_names[i]: [cast_ids[i], cast_chars[i], cast_profiles[i]] for i in range(len(cast_profiles))}
 
@@ -168,21 +192,24 @@ def recommend():
             movie_review_list = np.array([reviews.string])
             movie_vector = vectorizer.transform(movie_review_list)
             pred = clf.predict(movie_vector)
-            reviews_status.append('Good' if pred else 'Bad')
+            reviews_status.append('Positive' if pred else 'Negative')
+
+    # getting current date
+    movie_rel_date = ""
+    curr_date = ""
+    if (rel_date):
+        today = str(date.today())
+        curr_date = datetime.strptime(today, '%Y-%m-%d')
+        movie_rel_date = datetime.strptime(rel_date, '%Y-%m-%d')
 
     # combining reviews and comments into a dictionary
     movie_reviews = {reviews_list[i]: reviews_status[i] for i in range(len(reviews_list))}
 
     # passing all the data to the html file
     return render_template('recommend.html', title=title, poster=poster, overview=overview, vote_average=vote_average,
-                           vote_count=vote_count, release_date=release_date, runtime=runtime, status=status,
-                           genres=genres,
-                           movie_cards=movie_cards, reviews=movie_reviews, casts=casts, cast_details=cast_details)
-
-
-
-
-
+                           vote_count=vote_count, release_date=release_date, movie_rel_date=movie_rel_date,
+                           curr_date=curr_date, runtime=runtime, status=status, genres=genres, movie_cards=movie_cards,
+                           reviews=movie_reviews, casts=casts, cast_details=cast_details)
 
 
 # ==========================================================================================================
@@ -299,6 +326,13 @@ def logout():
     session.pop('user_id', None)
     flash('You have successfully logged out.')
     return redirect('/login')
+
+@app.route("/movierecommender")
+def movierecommender():
+    suggestions = get_suggestions()
+    # return render_template('movie_recommender_home.html', suggestions=suggestions,username=us)
+
+    return render_template('movie_recommender_home.html', suggestions=suggestions)
 
 
 if __name__ == '__main__':
